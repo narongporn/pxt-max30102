42380c5afd3870f79072187c8cd3ce635a873ba0
narongporn <43835385+narongporn@users.noreply.github.com>
2025-10-05 20:57:16 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,44 @@
-//% color=#E03131 weight=100 icon=""
-namespace max30102 {
-    let inited = false
+//% color=#AA0000 icon="♥" block="MAX30105"
+namespace max30105 {
 
-    //% shim=max30102_init
-    function _init(address: number): boolean { return false }
-
-    //% shim=max30102_readSample
-    function _readSample(outBuffer: number[]): number { return 0 }
-
-    //% shim=max30102_available
-    function _available(): number { return 0 }
-
-    //% shim=max30102_clearFifo
-    function _clearFifo(): void { }
-
-    //% block="MAX30102 initialize at I2C address %address"
-    //% address.defl=0x57
-    export function initialize(address: number = 0x57): boolean {
-        inited = _init(address)
-        return inited
+    //% block="initialize MAX30105"
+    export function begin(): boolean {
+        return _begin()
     }
 
-    //% block="MAX30102 read sample (returns 2-element array: [red, ir])"
-    export function readSample(): number[] {
-        if (!inited) initialize()
-        const out: number[] = [0, 0]
-        const got = _readSample(out)
-        if (got == 2) return out
-        return [0, 0]
+    //% block="read red value"
+    export function red(): number {
+        return _red()
     }
 
-    //% block="MAX30102 number of available samples"
-    export function available(): number {
-        if (!inited) initialize()
-        return _available()
+    //% block="read IR value"
+    export function ir(): number {
+        return _ir()
     }
 
-    //% block="MAX30102 clear FIFO"
-    export function clearFIFO(): void {
-        if (!inited) initialize()
-        _clearFifo()
+    //% block="read green value"
+    export function green(): number {
+        return _green()
     }
 
-    // Convenience blocks
-    //% block="MAX30102 read Red value"
-    export function readRed(): number {
-        const s = readSample()
-        return s[0]
+    //% block="read temperature °C"
+    export function temperature(): number {
+        return _temperature()
     }
 
-    //% block="MAX30102 read IR value"
-    export function readIR(): number {
-        const s = readSample()
-        return s[1]
-    }
+    // Shims
+    //% shim=max30105::_begin
+    function _begin(): boolean { return false }
+
+    //% shim=max30105::_red
+    function _red(): number { return 0 }
+
+    //% shim=max30105::_ir
+    function _ir(): number { return 0 }
+
+    //% shim=max30105::_green
+    function _green(): number { return 0 }
+
+    //% shim=max30105::_temperature
+    function _temperature(): number { return 0 }
 }
